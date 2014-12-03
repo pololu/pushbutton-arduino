76787ea4490389a9285651538a0392696f15cefc
David (Pololu) <dev-david@pololu.com>
2014-12-03 10:41:39 -0800

    Improved the emacs settings.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
-; This file causes the Emacs text editor to follow our indentation style.
+; Settings for the Emacs text editor.
 
 (
-  (c-mode . ((c-basic-offset . 2) (tab-width . 2) (indent-tabs-mode . nil)) )
+  (c-mode . ((c-basic-offset . 4) (tab-width . 4) (indent-tabs-mode . nil)))
+  (nil . ((fill-column . 80)))
+  ("USBPause/examples" . (
+    (nil . ((c-basic-offset . 2) (tab-width . 8) (fill-column . 65)))
+  ))
 )
