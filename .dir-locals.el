8c462d55582b2085add93f313ac1fa3bd88ee791
David (Pololu) <dev-david@pololu.com>
2014-12-23 10:47:29 -0800

    Fixed the directory name in .dir-locals.el.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
 (
   (c-mode . ((c-basic-offset . 4) (tab-width . 4) (indent-tabs-mode . nil)))
   (nil . ((fill-column . 80)))
-  ("USBPause/examples" . (
+  ("Pushbutton/examples" . (
     (nil . ((c-basic-offset . 2) (tab-width . 8) (fill-column . 65)))
   ))
 )
